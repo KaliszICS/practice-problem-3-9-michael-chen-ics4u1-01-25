c5844413b171383d2e02814dda06974be996ceb0
michael-chen-ics4u1-01-25 <michael.chen29@student.tdsb.on.ca>
2025-11-09 07:43:42 -0500

    working but may be error with tests

--- Notes.java
+++ Notes.java
@@ -1,6 +1,45 @@
+import java.util.ArrayList;
+import java.util.HashMap;
 
 class Notes {
 	public static void main(String[] args) {
+		towerOfHanoi(2);
+	}
 
+	public static String[] towerOfHanoi(int n) {
+		if (n == 0) {
+			return new String[]{};
+		}
+
+		HashMap<Integer, String> pegs = new HashMap<Integer, String>();
+		pegs.put(1, "L");
+		pegs.put(2, "M");
+		pegs.put(3, "R");
+
+		int auxiliary, dest;
+
+		if (n % 2 != 0) {
+			 auxiliary = 2;
+			 dest = 3;
+		} else {
+			auxiliary = 3;
+			dest = 2;
+		}
+
+		ArrayList<String> moves = new ArrayList<String>();
+
+		towerHelper(n, 1, auxiliary, dest, moves, pegs);
+
+		return moves;
+	}
+
+	public static void towerHelper(int n, int source, int auxiliary, int dest, ArrayList<String> moves, HashMap<Integer, String> pegs) {
+		if (n == 1) {
+			moves.add(pegs.get(source) + pegs.get(dest));
+			return;
+		}
+		towerHelper(n - 1, source, auxiliary, dest, moves, pegs);
+        moves.add(pegs.get(source) + pegs.get(dest));
+        towerHelper(n - 1, auxiliary, dest, source, moves, pegs);
 	}
 }
--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,27 +1,32 @@
-public class PracticeProblem {
+import java.util.ArrayList;
+import java.util.HashMap;
 
-	public static void main(String args[]) {
-
+class PracticeProblem {
+	public static void main(String[] args) {
+		for (String i : towerOfHanoi(4)) {
+			System.out.print(i + " ");
+		}
 	}
 
-	public static void q1() {
-		//Write question 1 code here
+	public static String[] towerOfHanoi(int n) {
+		if (n == 0) {
+			return new String[]{};
+		}
+
+		ArrayList<String> moves = new ArrayList<String>();
+
+		towerHelper(n, "L", "M", "R", moves);
+
+		return moves.toArray(new String[0]);
 	}
 
-	public static void q2() {
-		//Write question 2 code here
+	public static void towerHelper(int n, String source, String auxiliary, String dest, ArrayList<String> moves) {
+		if (n == 0) {
+			return;
+		}
+		towerHelper(n - 1, source, dest, auxiliary, moves);
+		System.out.println("Move " + (moves.size()) + ": " + source + " " + dest);
+        moves.add(source + dest);
+        towerHelper(n - 1, auxiliary, source, dest, moves);
 	}
-
-	public static void q3() {
-		//Write question 3 code here
-	}
-
-	public static void q4() {
-		//Write question 4 code here
-	}
-
-	public static void q5() {
-		//Write question 5 code here
-	}
-
 }
